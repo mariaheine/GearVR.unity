7843faf506bb01937d34645dab6d199b51001e81
BehemothTheCat <altanhir@gmail.com>
2018-02-24 16:16:58 +0100

    Initial commit; UCB test

---
+++ Assets/README.cs
@@ -0,0 +1,10 @@
+﻿namespace README
+{
+    /* 
+     * REQUIRED STEPS:
+     * Download android platform building from unity build settings.
+     * 
+     * Import Oculus Integration asset - what for?
+     * Import VR Samples asset - basic 
+     */
+}
